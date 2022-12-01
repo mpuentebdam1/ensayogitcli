b9781dfd41af08d57caa0f3ac6e95d9525f24504
Mario Puente <mpuentebdam1@gmail.com>
2022-12-01 08:18:57 +0100

    Modificado nombre de estudiante

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -16,7 +16,7 @@ public class TestEstudiante
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana", 18);
         e2.printEstudiante();
-        Estudiante e3 = new Estudiante("Elisa", 26);
+        Estudiante e3 = new Estudiante("Maite", 26);
         e3.printEstudiante();
         
 
